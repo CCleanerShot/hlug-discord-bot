307aaadb915131a634b8f153d3694b8d4d40d3f2
CCleanerShot <31040856+CCleanerShot@users.noreply.github.com>
2025-09-09 11:19:48 -0500

    fix: quick hardcode for ec2 timezone

--- src/discord/events/schedule.cs
+++ src/discord/events/schedule.cs
@@ -8,7 +8,7 @@ public partial class DiscordEvents
         // TODO: rewrite
         TimeSpan sevenDays = new TimeSpan(24) * 7;
         DateTime nowTime = new DateTime();
-        DateTime meetingTime = new DateTime(2025, 9, 2, 14, 0, 0, DateTimeKind.Utc);
+        DateTime meetingTime = new DateTime(2025, 9, 2, 19, 0, 0, DateTimeKind.Utc);
         TimeSpan result = meetingTime - nowTime;
 
         _HLUGTimer = new Timer(SendMessages, null, (int)result.TotalMilliseconds, (int)sevenDays.TotalMilliseconds);
