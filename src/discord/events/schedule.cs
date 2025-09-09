be4249fb711fea8312a3be2a9153298f2daabe16
CCleanerShot <31040856+CCleanerShot@users.noreply.github.com>
2025-09-09 14:55:26 -0500

    fix: hardcode. remind me to fix that

--- src/discord/events/schedule.cs
+++ src/discord/events/schedule.cs
@@ -8,7 +8,7 @@ public partial class DiscordEvents
         // TODO: rewrite
         TimeSpan sevenDays = new TimeSpan(24) * 7;
         DateTime nowTime = new DateTime();
-        DateTime meetingTime = new DateTime(2025, 9, 2, 19, 0, 0, DateTimeKind.Utc);
+        DateTime meetingTime = new DateTime(2025, 9, 9, 20, 0, 0, DateTimeKind.Utc);
         TimeSpan result = meetingTime - nowTime;
 
         _HLUGTimer = new Timer(SendMessages, null, (int)result.TotalMilliseconds, (int)sevenDays.TotalMilliseconds);
@@ -17,6 +17,6 @@ public partial class DiscordEvents
     public async void SendMessages(object? state)
     {
         ITextChannel channel = (ITextChannel)await DiscordBot._Client.GetChannelAsync(UInt64.Parse(Environment.GetEnvironmentVariable("DISCORD_TEST_CHANNEL")!));
-        await channel.SendMessageAsync($"<@{Environment.GetEnvironmentVariable("DISCORD_TEST_ROLE_ID")}> HLUG starts in 4 hours! See you there!");
+        await channel.SendMessageAsync($"<@{Environment.GetEnvironmentVariable("DISCORD_TEST_ROLE_ID")}> HLUG starts in 3 hours! See you there!");
     }
 }
